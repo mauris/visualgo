8591573dc48ab709f5517195155e94df4bc53fa8
Rose Marie Tan <rose.marie.tzy@gmail.com>
2014-01-23 12:25:09 +0800

    graph traversal articulation pt qn added

--- php/GraphTraversal.php
+++ php/GraphTraversal.php
@@ -102,33 +102,40 @@ class GraphTraversal {
 	}
 	
 	//returns an array of integers - the vertices that when removed, will cause a disconnect
-	public function disconnect() {
+	public function disconnect() {		
 		$ans = array();
 		
-		$stack = array();
-		$visited = array(); //-1 for undiscovered, otherwise discovery time
-		$low = array();
 		$keys = $this->getAllElements();
-		for($i=0; $i<count($keys); $i++) {
-			$visited[$keys[$i]] = -1;
-			$low[$keys[$i]] = INFINITY;
-		}
-		
-		$time = 1;
-		$stack[] = $start;
-		while(!empty($stack)) {
-			$u = array_pop($stack);
-			if($visited[$u] ==  -1) {
-				$visited[$u] = $time++;
-				$low[$u] = min($low[$u], $visited[$u]);
-				$nNeighbours = count($this->adjList[$u]);
-				for($i=($nNeighbours-1); $i>=0; $i--) {
-					$v = $this->adjList[$u][$i]->v();
-					$stack[] = $v;
+		for($i=0; $i<count($keys); $i++) { //for each vertex
+			$ignore = $keys[$i];
+			//do dfs on the graph without this vertex (virtually remove this vertex from the graph)
+			$stack = array();
+			$visited = array();
+			$traversal = array();
+			for($j=0; $j<count($keys); $j++) {
+				$visited[$keys[$j]] = false;
+			}
+			if($i == 0) {
+				$stack[] = $keys[count($keys)-1];
+			} else {
+				$stack[] = $keys[0];
+			}
+			while(!empty($stack)) {
+				$u = array_pop($stack);
+				if(!$visited[$u] && $u != $ignore) { //add extra condition to ignore this vertex
+					$visited[$u] = true;
+					$traversal[] = $u;
+					$nNeighbours = count($this->adjList[$u]);
+					for($k=($nNeighbours-1); $k>=0; $k--) {
+						$v = $this->adjList[$u][$k]->v();
+						$stack[] = $v;
+					}
 				}
 			}
+			if(count($traversal) < (count($keys)-1)) { //causes disconnect
+				$ans[] = $ignore;
+			}
 		}
-		
 		return $ans;
 	}
 	
