159e64c75134b1a1b30a7f27069ea10dc85975bd
Rose Marie Tan <rose.marie.tzy@gmail.com>
2014-02-13 23:11:41 +0800

    updated control panel to use new modes

--- js/test/controlpanel.js
+++ js/test/controlpanel.js
@@ -27,13 +27,13 @@ function loadTraining() {
 }
 
 function startTraining() {
-	init();
 	$.ajax({
 		url: "php/Test.php?mode="+MODE_GENERATE_QUESTIONS+"&qAmt="+nQns+"&seed="+seed+"&topics="+topics.toString()
 	}).done(function(data) {
 		MODE = "TRAINING";
 		
 		data = JSON.parse(data);
+		init();
 		for(var i=1; i<=nQns; i++) {
 			extractInfo(i, data[i-1]);
 		}
@@ -58,6 +58,7 @@ function submitTraining() {
 	//get score
 	ansArr.shift();
 	var ansStr = ansArr.join('&ans[]=');
+	ansArr.unshift(false);
 	var queryStr = "php/Test.php?mode="+MODE_CHECK_ANSWERS+"&ans[]="+ansStr+"&seed="+seed+"&qAmt="+nQns+"&topics="+topics.toString();
 	console.log(queryStr); //to remove later
 	$.ajax({
@@ -67,6 +68,35 @@ function submitTraining() {
 		$('#score').html(score+" out of "+nQns);
 		$('#test-screen').fadeOut("fast");
 		$('#result-screen').fadeIn("fast");
+	});
+}
+
+function startAns() {
+	ansArr.shift();
+	var ansStr = ansArr.join('&ans[]=');
+	ansArr.unshift(false);
+	var queryStr = "php/Test.php?mode="+MODE_GET_ANSWERS+"&ans[]="+ansStr+"&seed="+seed+"&qAmt="+nQns+"&topics="+topics.toString();
+	console.log("http://algorithmics.comp.nus.edu.sg/~onlinequiz/training/"+queryStr); //to remove later
+	$.ajax({
+		url: queryStr
+	}).done(function(ansData) {
+		//store into anskeyArr array
+		ansData = JSON.parse(ansData);
+		for(var i=0; i<ansData.length; i++) {
+			anskeyArr[i+1] = ansData[i];
+		}
+
+		$('#result-screen').fadeOut('fast');
+		$('#test-screen').fadeIn('fast');
+		$('#ans-key').show();
+		$('#undo-ans').hide();
+		$('#clear-ans').hide();
+		$('#info').hide();
+				
+		$('#question-nav .qnno').removeClass('selected');
+		$('#question-nav .qnno').eq(0).addClass('selected');
+		qnNo = 1; //start with qn 1
+		showQn(qnNo);
 	});
 }
 
@@ -394,18 +424,6 @@ $(document).ready (function() {
 	});
 	$('#goto-answer').click(function() {
 		MODE = "ANSWER";
-		$('#result-screen').fadeOut('fast');
-		$('#test-screen').fadeIn('fast');
-		$('#ans-key').show();
-		$('#undo-ans').hide();
-		$('#clear-ans').hide();
-		$('#info').hide();
-		
-		ansArr.unshift(false);
-		
-		$('#question-nav .qnno').removeClass('selected');
-		$('#question-nav .qnno').eq(0).addClass('selected');
-		qnNo = 1; //start with qn 1
-		showQn(qnNo);
+		startAns();
 	});	
 });
