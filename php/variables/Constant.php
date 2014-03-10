b46f7eec4c7dc690f24c29522cb9a93ace9298ea
Ivan Reinaldo <ivan.reinaldo92@gmail.com>
2014-03-10 16:11:33 +0800

    Moved graph template names

--- php/variables/Constant.php
+++ php/variables/Constant.php
@@ -154,17 +154,5 @@
   const GRAPH_TEMPLATE_TYPE_DIRECTED = 0;
   const GRAPH_TEMPLATE_TYPE_UNDIRECTED = 1;
 
-  const GRAPH_TEMPLATE_EMPTY = "empty";
-  const GRAPH_TEMPLATE_K5 = "k5";
-  const GRAPH_TEMPLATE_TESSELLATION = "tesselation";
-  const GRAPH_TEMPLATE_RAIL = "rail";
-  const GRAPH_TEMPLATE_CP4P10 = "cp4.10";
-
-  const GRAPH_TEMPLATE_CP3_4_17 = "cp4.17";
-  const GRAPH_TEMPLATE_CP3_4_18 = "cp4.18";
-  const GRAPH_TEMPLATE_CP3_4_19 = "cp4.19";
-  const GRAPH_TEMPLATE_BIDIRECTED_1 = "bidirected1";
-  const GRAPH_TEMPLATE_DIRECTED_1 = "directed1";
-
   const INFINITY = 1000000000;
 ?>
